9ba3a345c00d479ac17f0cbae3cdbdfc3a0a1c56
Mr.MichaelSchetzer@gmail.com <Michael Schetzer>
2023-06-28 21:21:15 +0300

    you can type any message you want here7

--- src/components/Aboutpage/Aboutpage.js
+++ src/components/Aboutpage/Aboutpage.js
@@ -10,30 +10,26 @@ function Aboutpage() {
                 <Row className='textbackground'>
                     <Col md={7} >
                         <h3 className='aboutmetext'>About <span>Me</span></h3>
-<<<<<<< HEAD
-                        <p className='aboutdetails'>I'm a self-taught Front End developer from Israel. I build websites with a focus on providing the experience for everyone using them and responsiveness. Curious to learn more about developing scalable distribution systems, love problem solving, and care about writing and maintainable code. A responsive design makes your website accessible to all users, regardless of their devices.
-=======
-                        <p className='aboutdetails'>I'm a self-taught Front End developer based in Israel. I build websites with a focus on providing the experience for everyone using them and responsiveness. Curious to learn more about developing scalable distribution systems, love problem solving, and care about writing and maintainable code. A responsive design makes your website accessible to all users, regardless of their devices.
->>>>>>> 75fbe7a (my changes)
-                        </p>
-                        <ul className='skilllist'>
-                            <Row>
-                                <h3>Skills</h3>
-                                <Col md={7}>
-                                    <li>HTML5/CSS3</li>
-                                    <li>JavaScript</li>
-                                    <li>jQuery</li>
-                                    <li>Bootsrap 5</li>
-                                </Col>
-                                <Col md={5}>
-                                    <li>React Js</li>
-                                    <li>Redux Js</li>
-                                    <li>React-Bootsrap</li>
-                                    <li>Material-ui</li>
-                                    <li>Git/Github</li>
-                                </Col>
-                            </Row>
-                        </ul>
+                            <p className='aboutdetails'>I'm a self-taught Front End developer based in Israel. I build websites with a focus on providing the experience for everyone using them and responsiveness. Curious to learn more about developing scalable distribution systems, love problem solving, and care about writing and maintainable code. A responsive design makes your website accessible to all users, regardless of their devices.
+                            </p>
+                            <ul className='skilllist'>
+                                <Row>
+                                    <h3>Skills</h3>
+                                    <Col md={7}>
+                                        <li>HTML5/CSS3</li>
+                                        <li>JavaScript</li>
+                                        <li>jQuery</li>
+                                        <li>Bootsrap 5</li>
+                                    </Col>
+                                    <Col md={5}>
+                                        <li>React Js</li>
+                                        <li>Redux Js</li>
+                                        <li>React-Bootsrap</li>
+                                        <li>Material-ui</li>
+                                        <li>Git/Github</li>
+                                    </Col>
+                                </Row>
+                            </ul>
                     </Col>
                     <Col md={5}>
                         <div className="webimage"></div>
